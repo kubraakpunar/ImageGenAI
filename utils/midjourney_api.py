651ebf9bbc6b95f6b49c90089324e16126089658
kubraakpunar <kubraakpunarb@gmail.com>
2024-08-06 12:28:03 +0300

    refactor: wait time fix

--- utils/midjourney_api.py
+++ utils/midjourney_api.py
@@ -66,7 +66,7 @@ class MidjourneyApi:
             "Content-Type": "application/json",
         }
         for i in range(5):
-            time.sleep(15)
+            time.sleep(20)
             try:
                 response = requests.get(f'https://discord.com/api/v9/channels/{self.channel_id}/messages', headers=headers)
                 if response.status_code != 200:
